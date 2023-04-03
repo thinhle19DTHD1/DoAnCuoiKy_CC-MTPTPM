75b37a48875c748f1a361df362f4cbf7c4ebe9ec
thin2201 <phantatthin2201@gmail.com>
2023-04-03 10:37:27 +0700

    'updatecopy'

---
+++ PTT copy.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+
+</head>
+<body>
+<?php
+    echo "Phan Tất Thìn";
+    echo "1911065408";
+    echo "Phan Tất Thìn";
+    echo "1911065408";
+?>
+</body>
+</html>
