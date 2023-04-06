4d64717b5748ee7617de405afa84938d37246ea9
Thinh <thinhletruongquoc66@gmail.com>
2023-04-06 23:26:50 +0700

    update index

--- index.php
+++ index.php
@@ -1,115 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Thời trang chính hãng</title>
-</head>
-<body>
-    <div class="wrapper"> 
-    <div class="header"></div>
-    <div class="menu"> 
-            <ul class="list_menu">
-                <li><a href="index.php">Trang chủ </a></li>
-                <li><a href="index.php?quanly=danhmucsanpham&id=1">Danh mục sản phẩm </a></li>
-                <li><a href="index.php?quanly=giohang">Giỏ hàng </a></li>
-                <li><a href="index.php?quanly=tintuc">Tin tức </a></li>
-                <li><a href="index.php?quanly=lienhe">Liên hệ </a></li>
-            </ul>
-</div>
-<div id="main">
-            <div class="sidebar">
-                <ul class="list_sidebar">
-
-                    <li><a href="#">Quần áo thể thao </a></li>
-                    <li><a href="#">Giày </a></li>
-                    <li><a href="#">Dép </a></li>
-                    <li><a href="#">Áo khoác </a></li>
-                    <li><a href="#">Vòng đeo cổ </a></li>
-                    <li><a href="#">vòng tay </a></li>
-                    <li><a href="#">Đồng hồ </a></li>
-                    <li><a href="#">Nhẫn nam </a></li>
-                    <li><a href="#">túi xách </a></li>
-
-
-                </ul>
-            </div>
-
-            <div class="maincontent">
+<h3>Sản phẩm mới nhất</h3>
                 <ul class="product_list">
                     <li>
                         <a href="">
                             <img src="images/black_sneacker.jpg">
-                            <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                            <p class="price_product">giá: 480000 vnd</p>
+                            <p class="title_product">Giày sneaker</p>
+                            <p class="price_product">giá: 479000 vnd</p>
                         </a>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/kenta.jpg">
+                        <p class="title_product">Set Kenta thời trang đi du lịch</p>
+                        <p class="price_product">giá: 719000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/quytoc.jpg">
+                        <p class="title_product">Vest xám phối sơ mi trắng</p>
+                        <p class="price_product">giá: 1499000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/jogger.jpg">
+                        <p class="title_product">Quấn Jogger Kaki</p>
+                        <p class="price_product">giá: 279000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/sweater.jpg">
+                        <p class="title_product">Sweater nam</p>
+                        <p class="price_product">giá: 359000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/short.jpg">
+                        <p class="title_product">Quần short thun co dãn</p>
+                        <p class="price_product">giá: 179000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/hoodie.jpg">
+                        <p class="title_product">Áo hoodie ấm</p>
+                        <p class="price_product">giá: 349000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/sneacker_sport.jpg">
+                        <p class="title_product">Giày thể thao</p>
+                        <p class="price_product">giá: 379000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/polo.jpg">
+                        <p class="title_product">Áo Polo</p>
+                        <p class="price_product">giá: 249000 vnd</p>
                     </li>
                     <li>
                         <a href="">
-                        <img src="images/black_sneacker.jpg">
-                        <p class="title_product">Tên sản phẩm: Giày sneaker</p>
-                        <p class="price_product">giá: 480000 vnd</p>
+                        <img src="images/jean.jpg">
+                        <p class="title_product">Jean nam ống suông</p>
+                        <p class="price_product">giá: 379000 vnd</p>
                     </li>
                     
+                    
                 </ul>
-            </div>    
-</div>
-
-
-
-
-
-
-</div>
-</body>
-</html>
